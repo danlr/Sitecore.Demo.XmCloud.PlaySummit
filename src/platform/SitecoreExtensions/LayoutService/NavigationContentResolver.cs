74b5f686e27c41d2e9bac143a24beb2e889ea486
Andrei Harshunou <andrei.gorshunov@gmail.com>
2025-05-19 12:54:13 +0200

    fix error

--- src/platform/SitecoreExtensions/LayoutService/NavigationContentResolver.cs
+++ src/platform/SitecoreExtensions/LayoutService/NavigationContentResolver.cs
@@ -1,10 +1,17 @@
-﻿namespace Sitecore.Demo.Edge.Website.SitecoreExtensions.LayoutService
+﻿using Sitecore.LayoutService.Configuration;
+using Sitecore.Mvc.Presentation;
+
+namespace Sitecore.Demo.Edge.Website.SitecoreExtensions.LayoutService
 {
     public class NavigationContentResolver : Sitecore.XA.JSS.Feature.Navigation.ContentsResolvers.NavigationContentResolver
     {
-        public NavigationContentResolver()
+        public new object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
         {
+            var result = base.ResolveContents(rendering, renderingConfig);
+
             this.UrlOptions.Language = Sitecore.Context.Item.Language;
+
+            return result;
         }
     }
 }
