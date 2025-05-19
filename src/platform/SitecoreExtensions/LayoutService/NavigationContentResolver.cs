d2bedd07c561d2e048416c80aaf40c59661bb61f
Andrei Harshunou <andrei.gorshunov@gmail.com>
2025-05-19 13:34:37 +0200

    add logging

--- src/platform/SitecoreExtensions/LayoutService/NavigationContentResolver.cs
+++ src/platform/SitecoreExtensions/LayoutService/NavigationContentResolver.cs
@@ -6,10 +6,13 @@ namespace Sitecore.Demo.Edge.Website.SitecoreExtensions.LayoutService
 {
     public class NavigationContentResolver : Sitecore.XA.JSS.Feature.Navigation.ContentsResolvers.NavigationContentResolver
     {
-        protected virtual NavigationItemModel CreateNavigationModel(Item item, int level, int index, int siblingCount, List<NavigationItemModel> children = null, int flatLevel = -1)
+        protected override NavigationItemModel CreateNavigationModel(Item item, int level, int index, int siblingCount, List<NavigationItemModel> children = null, int flatLevel = -1)
         {
             this.UrlOptions.Language = Sitecore.Context.Item.Language;
-            
+
+            Sitecore.Diagnostics.Log.Info($"Context langauge: {this.UrlOptions.Language.Name}", this);
+            Sitecore.Diagnostics.Log.Info($"Url: {this.LinkProvider.GetItemUrl(item, this.UrlOptions)}", this);
+
             return base.CreateNavigationModel(item, level, index, siblingCount, children, flatLevel); 
         }
     }
