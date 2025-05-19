d55556e45d6a0a1963456878a338eb78810cf26e
Andrei Harshunou <andrei.gorshunov@gmail.com>
2025-05-19 13:10:56 +0200

    resolve language

--- src/platform/SitecoreExtensions/LayoutService/NavigationContentResolver.cs
+++ src/platform/SitecoreExtensions/LayoutService/NavigationContentResolver.cs
@@ -1,17 +1,16 @@
-﻿using Sitecore.LayoutService.Configuration;
-using Sitecore.Mvc.Presentation;
+﻿using Sitecore.XA.JSS.Feature.Navigation.Models;
+using System.Collections.Generic;
+using Sitecore.Data.Items;
 
 namespace Sitecore.Demo.Edge.Website.SitecoreExtensions.LayoutService
 {
     public class NavigationContentResolver : Sitecore.XA.JSS.Feature.Navigation.ContentsResolvers.NavigationContentResolver
     {
-        public new object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        protected virtual NavigationItemModel CreateNavigationModel(Item item, int level, int index, int siblingCount, List<NavigationItemModel> children = null, int flatLevel = -1)
         {
-            var result = base.ResolveContents(rendering, renderingConfig);
-
             this.UrlOptions.Language = Sitecore.Context.Item.Language;
-
-            return result;
+            
+            return base.CreateNavigationModel(item, level, index, siblingCount, children, flatLevel); 
         }
     }
 }
